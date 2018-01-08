41dc67cfeebe779165685a837d8bf93a6f306eda
vicmar57 <vicmar57@gmail.com>
2018-01-09 00:21:27 +0200

    almost done. working quite well still.

--- Radio/src/radio_control.c
+++ Radio/src/radio_control.c
@@ -35,9 +35,10 @@ ID: 302745146
 #define 	SUCCESS					1
 #define 	upload_delay			8000 //set the upload rate at the client to 1KiB every 8000 usec.
 #define 	kiB						1024
-#define 	max_uint16_t			65535
+#define 	max_num_stations			65535
 
 #define 	server_is_idiot			1
+
 
 //global variables
 int state=0, upload=0, UDP_closed =0, change_station=0, current_station=0,remeinder=0 , TCP_Sock = 0 ,msg_sent = 0 ,multicastGroup;
@@ -51,22 +52,15 @@ struct timeval tv;
 fd_set rfds;
 struct in_addr reference_mCast_addr;
 struct in_addr current_mCast_addr;
-uint16_t port;
-
-/*typedef struct {
-	uint8_t replyType;
-	uint16_t numStations;
-	char mGroup_IP [maxIPlen];
-	uint16_t port;
-} Welcome_msg;
 
 typedef struct {
 	char mGroup_IP [maxIPlen];
-	int mGroup_port;
-} UDP_sock_data;*/
+	uint16_t mGroup_port;
+} UDP_sock_data;
 
-int setTCP_sock(char* serv_ip, int serv_port);
-int handShake();
+
+UDP_sock_data setTCP_sock(char* serv_ip, int serv_port);
+UDP_sock_data handShake();
 void* listener(void* UDP);
 void handle_TCP_and_IO();
 int handle_TCP_message();
@@ -75,6 +69,7 @@ int uploadSong();
 
 int main(int argc, char* argv[]) // Input: SERVER's IP_address/NAME, SERVER's TCP WELCOME port.
 {
+	UDP_sock_data udpData;
 	int SERVER_TCP_port;
 	char * SERVER_TCP_IP;
 	struct hostent *hp = gethostbyname(argv[1]); //get real ip address of host, from /etc/hosts file
@@ -82,16 +77,16 @@ int main(int argc, char* argv[]) // Input: SERVER's IP_address/NAME, SERVER's TC
 	if (hp == NULL)
 	{ printf("Hostname not found in /etc/hosts file. quitting program.\n"); exit(ERROR); }
 	else
-		SERVER_TCP_IP = inet_ntoa(*(struct in_addr*)( hp -> h_addr_list[0]));
+		SERVER_TCP_IP = inet_ntoa(*(struct in_addr*)( hp -> h_addr_list[0])); //string ip address
 
 	//Check that we got from the user all of the arguments to activate the server
 	printf("arguments received - exe File: %s, Hostname: %s (TCP IP: %s), TCP port: %s,\n(total of %d args).\n", argv[0], argv[1],SERVER_TCP_IP, argv[2] , argc);
 	if(argc != 3) { printf("Too few/many arguments received. Bye bye!\n"); exit(ERROR); }
 
 	SERVER_TCP_port = atoi(argv[2]); //get TCP port from input
-	setTCP_sock(SERVER_TCP_IP,SERVER_TCP_port);
+	udpData = setTCP_sock(SERVER_TCP_IP,SERVER_TCP_port);
 
-	if(pthread_create(&t, NULL, listener, NULL)!=0) //create listener thread (udp multicast - song play)
+	if(pthread_create(&t, NULL, listener, &udpData)!=0) //create listener thread (udp multicast - song play)
 	{printf("\n\nerror in thread. exiting.");close(TCP_Sock); exit(ERROR);}
 	handle_TCP_and_IO();
 
@@ -103,20 +98,19 @@ int main(int argc, char* argv[]) // Input: SERVER's IP_address/NAME, SERVER's TC
 	return EXIT_SUCCESS;
 }
 
-Welcome_msg handShake()
+UDP_sock_data handShake()
 {
-	Welcome_msg welcome_msg = {0};
+	UDP_sock_data welc_msg = {0};
 	int rec_bytes = 0, retval=0;
 	unsigned char hello_msg [3] = {0,0,0} , welc_buff [9];
 
 	FD_ZERO(&rfds);
 	FD_SET(TCP_Sock, &rfds);
-	//set time out's
-	tv.tv_sec = 0; // wait for 0.2 sec for the WELCOME MSG
+	tv.tv_sec = 0; // set timeouts -wait for 0.2 sec for the WELCOME MSG
 	tv.tv_usec = 200000;
 
 	if(send(TCP_Sock,hello_msg,sizeof(hello_msg),0) == -1)
-	{ perror(""); printf("\nError in sending hello message"); return welcome_msg;}
+	{ perror(""); printf("\nError in sending hello message"); return welc_msg;}
 
 	retval = select(TCP_Sock+1, &rfds, NULL, NULL, &tv); // Watch stdin and tcp_sock to see when it has input.
 	if (retval == -1) { perror("select() failed in upsong"); state = OFF_INIT; }
@@ -131,41 +125,33 @@ Welcome_msg handShake()
 		close (TCP_Sock);
 		exit(0);
 	}
-
 	state = WAIT_WELCOME;
 
 	rec_bytes = recv(TCP_Sock, welc_buff, sizeof(welc_buff) , 0);
 	if (rec_bytes == -1)
-	{ perror(""); printf("\nError in sending hello message"); return welcome_msg;}
+	{ perror(""); printf("\nError in sending hello message"); return welc_msg;}
 	else if (rec_bytes != 9)
-	{ printf("\nError in welcome message - size incorrect"); return welcome_msg;}
+	{ printf("\nError in welcome message - size incorrect"); return welc_msg;}
 
-	welcome_msg.replyType = (uint8_t)welc_buff[0];
-	welcome_msg.numStations = ntohs(((uint16_t*)(welc_buff+1))[0]);
-	NumStations = welcome_msg.numStations;
+	NumStations = ntohs(((uint16_t*)(welc_buff+1))[0]); //SET NUM OF RADIO STATIONS.
 
-	sprintf(welcome_msg.mGroup_IP,"%d.%d.%d.%d",welc_buff[6],welc_buff[5],welc_buff[4],welc_buff[3]);	// get multicast address from buffer
-	inet_aton(welcome_msg.mGroup_IP, &reference_mCast_addr);
+	sprintf(welc_msg.mGroup_IP,"%d.%d.%d.%d",welc_buff[6],welc_buff[5],welc_buff[4],welc_buff[3]);	// get multicast address from buffer
+	inet_aton(welc_msg.mGroup_IP, &reference_mCast_addr);
+	current_mCast_addr = reference_mCast_addr;
 
-	printf("\n%s\n", inet_ntoa(reference_mCast_addr));
-	reference_mCast_addr.s_addr=(uint32_t)ntohl((htonl((reference_mCast_addr.s_addr)))+1);
-	printf("%s\n", inet_ntoa(reference_mCast_addr));
-
-
-	welcome_msg.port = ntohs(((uint16_t*)(welc_buff+7))[0]);
+	welc_msg.mGroup_port = ntohs(((uint16_t*)(welc_buff+7))[0]);
 
 	state = ESTABLISHED;
-	return welcome_msg;
+	return welc_msg;
 }
 
 void* listener(void* UDP)
 {
-	UDP_sock_data UDP_data = *(UDP_sock_data*) UDP;
+	UDP_sock_data udp_data = *(UDP_sock_data*)UDP;
 	struct ip_mreq mreq;
-	int UDP_sock,UDP_pack_len =1, reuse = 1, last_octet=0;
+	int UDP_sock,UDP_pack_len =1, reuse = 1;
 	struct sockaddr_in udp_serverAddr = {0};
 	char  message[Buffer_size];
-	char ip [maxIPlen];
 	FILE * fp;
 	socklen_t addr_len;
 
@@ -175,7 +161,7 @@ void* listener(void* UDP)
 	//define the port and the multicast IP to send the data on
 	udp_serverAddr.sin_family = AF_INET;
 	udp_serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	udp_serverAddr.sin_port = htons(UDP_data.mGroup_port);
+	udp_serverAddr.sin_port = htons(udp_data.mGroup_port);
 	addr_len = sizeof udp_serverAddr;
 
 	setsockopt(UDP_sock, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse));
@@ -183,10 +169,7 @@ void* listener(void* UDP)
 	if(bind(UDP_sock, (struct sockaddr *) &udp_serverAddr, sizeof(udp_serverAddr))==-1)
 	{ perror("Can't bind UDP socket"); close(UDP_sock); exit(ERROR); }
 
-/*sprintf(ip,"%d.%d.%d.%d",(UDP_data.mGroup_IP)[0], (UDP_data.mGroup_IP)[1], (UDP_data.mGroup_IP)[2], (UDP_data.mGroup_IP)[3]);	// get multicast address from buffer
-	last_octet = (UDP_data.mGroup_IP)[3]; //TODO*/
-
-	mreq.imr_multiaddr.s_addr = inet_addr(UDP_data.mGroup_IP);
+	mreq.imr_multiaddr.s_addr = inet_addr(inet_ntoa(reference_mCast_addr));
 	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
 	setsockopt(UDP_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)); //join mGroup
 
@@ -198,14 +181,8 @@ void* listener(void* UDP)
 		if(change_station)
 		{
 			setsockopt(UDP_sock, IPPROTO_IP, IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq)); //DROP mGroup MEMBERSHIP
-
 			current_mCast_addr.s_addr = (uint32_t)ntohl((htonl((reference_mCast_addr.s_addr))) + current_station);
-
-			/*(UDP_data.mGroup_IP)[3] = last_octet + current_station; //todo
-			sprintf(ip,"%d.%d.%d.%d",(UDP_data.mGroup_IP)[0], (UDP_data.mGroup_IP)[1], (UDP_data.mGroup_IP)[2], (UDP_data.mGroup_IP)[3]);
-			*/
-
-			mreq.imr_multiaddr.s_addr = inet_addr(ip);
+			mreq.imr_multiaddr.s_addr = inet_addr(inet_ntoa(current_mCast_addr));
 			setsockopt(UDP_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)); //join mGroup
 			change_station = 0;
 		}
@@ -223,7 +200,7 @@ void* listener(void* UDP)
 		}
 	}
 
-	//state == OFF_INIT
+	//state == OFF_INIT - finalize connection
 	setsockopt(UDP_sock, IPPROTO_IP, IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq)); //DROP mGroup MEMBERSHIP
 	printf("closing streaming communication! Bye bye!\n");
 
@@ -231,15 +208,14 @@ void* listener(void* UDP)
 	close(UDP_sock);
 	UDP_closed = 1;
 	pthread_exit(&t);
-	//return NULL;
 }
 
 int handle_user_input()
 {
 	int Song_name_size,song_size,i=1;
-	uint8_t len;
 	unsigned char askSong_msg [3] = {1,0,0};
 	char* upSong_msg;
+	uint16_t var = 0;
 	FILE * songP;
 	DIR *d;
 	struct dirent *dir;
@@ -255,7 +231,7 @@ int handle_user_input()
 			{
 				if(strstr(dir->d_name, ".mp3") != NULL)
 				{
-					printf(" %d) %s", i, dir->d_name);
+					printf(" (%d) %s", i, dir->d_name);
 					i++;
 				}
 			}
@@ -321,25 +297,22 @@ int handle_user_input()
 		state = OFF_INIT;
 		return SUCCESS;
 	}
-	else if ((uint16_t)atoi(user_input) >= 0 && (uint16_t)atoi(user_input) < max_uint16_t) //&& (uint8_t)atoi(user_input)<NumStations)//entered a number
+	else if ((uint16_t)atoi(user_input) >= 0 && (uint16_t)atoi(user_input) < max_num_stations) //&& (uint8_t)atoi(user_input)<NumStations)//entered a number
 	{
 		if((uint16_t)atoi(user_input) == 0 && strcmp(user_input, "0") != 0) // not zero! input is LETTERS. invalid input!!
 		{
 			printf("bad command. please try again.\n");
 		}
-		else if ((uint16_t)atoi(user_input)<NumStations)//entered a good number
+		else if ((uint16_t)atoi(user_input) < NumStations)//entered a good number
 		{
-			len = (unsigned)strlen(user_input); //TODO handle 2 byte number.
-			if(len==1)// && (*user_input >= 0 && *user_input <= NumStations))
-			{
-				askSong_msg[2] = (uint8_t)atoi(user_input);
-				current_station = (int)atoi(user_input);
-				if(send(TCP_Sock,askSong_msg,sizeof(askSong_msg),0) == -1)
-				{ perror(""); printf("\nError in sending askSong message");}
-				else
-					msg_sent = 1;
-				state = WAIT_ANNOUNCE;
-			}
+			//todo
+			askSong_msg[2] = (uint16_t)atoi(user_input); //insert requested station number to upSong msg
+			current_station = (int)atoi(user_input);
+			if(send(TCP_Sock,askSong_msg,sizeof(askSong_msg),0) == -1)
+			{ perror(""); printf("\nError in sending askSong message");}
+			else
+				msg_sent = 1; //flag to check for timeout
+			state = WAIT_ANNOUNCE;
 		}
 		else
 			printf("station %d doesn't exist. please try again.\n",(uint16_t)atoi(user_input));
@@ -349,10 +322,8 @@ int handle_user_input()
 
 UDP_sock_data setTCP_sock (char* serv_ip, int serv_port)
 {
-	int i;
 	struct sockaddr_in serverAddr;
-	UDP_sock_data sock_info; //UDP socket info
-	Welcome_msg welc = {0};
+	UDP_sock_data udp_data;
 	// Create the TCP&UDP socket and set it’s properties. The three arguments are: 1) Internet domain. 2) Stream socket. 3) Default protocol (TCP in this case).
 	TCP_Sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (TCP_Sock == -1)  { perror("Can't create TCP socket"); close(TCP_Sock); exit(ERROR); }
@@ -367,8 +338,8 @@ UDP_sock_data setTCP_sock (char* serv_ip, int serv_port)
 	if(connect(TCP_Sock, (struct sockaddr *) &serverAddr, sizeof(serverAddr))== -1)
 	{ perror("Can't connect to server"); close(TCP_Sock); exit(ERROR); }
 
-	welc = handShake(TCP_Sock); //handshake with server, get relevant data from it.
-	if(welc.numStations == 0 && welc.mGroup_IP==0 && welc.port==0) //no real data received
+	udp_data = handShake(TCP_Sock); //handshake with server, get relevant data from it.
+	if(strcmp(udp_data.mGroup_IP,"000000000000000") == 0 && udp_data.mGroup_port== 0) //no real data received
 	{
 		printf("error in welcome message! quitting program.");
 		close(TCP_Sock);
@@ -376,13 +347,8 @@ UDP_sock_data setTCP_sock (char* serv_ip, int serv_port)
 	}
 	else
 		fprintf(stdout,"connection established. data received from server:\n%d stations, mGroup IP: %s, mGroup port: %d. playing"
-				" station 0.",welc.numStations,welc.mGroup_IP,welc.port);
-
-	strcpy(sock_info.mGroup_IP, welc.mGroup_IP);
-
-	sock_info.mGroup_port = welc.port;
-
-	return sock_info; //return UDP socket info
+				" station 0.",NumStations,udp_data.mGroup_IP,udp_data.mGroup_port);
+	return udp_data; //return UDP socket info
 }
 
 void handle_TCP_and_IO()
@@ -532,11 +498,10 @@ int handle_TCP_message()
 	{ perror("Error in reading from TCP socket"); printf("\n"); }
 	else if (TCP_pack_len == 0)
 	{
-		printf("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~Connection closed by server.~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+		printf("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~TCP Connection closed by server.~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 		state = OFF_INIT;
 		return ERROR;
 	}
-
 	return SUCCESS;
 }
 
@@ -627,6 +592,11 @@ int uploadSong() //, int parts, char* song_file, int song_size)  	//UPLOAD THE S
 
 
 /*
+ * 	printf("\n%s\n", inet_ntoa(reference_mCast_addr));
+	reference_mCast_addr.s_addr=(uint32_t)ntohl((htonl((reference_mCast_addr.s_addr)))+1);
+	printf("%s\n", inet_ntoa(reference_mCast_addr));
+ *
+ *
  *		//if((access(user_input, F_OK) == file_exists) && (access(user_input, R_OK ) == file_readable)) // file exists and readable.send upsong message to server.
  */
 
